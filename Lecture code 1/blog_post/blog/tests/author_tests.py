cd40ad6a41899061a9155a8ef02f746150562560
MariamKipshidze <mari.kifshidze@gmail.com>
2025-10-20 20:47:36 +0400

    Add AuthorAPITest

--- Lecture code 1/blog_post/blog/tests/author_tests.py
+++ Lecture code 1/blog_post/blog/tests/author_tests.py
@@ -1,6 +1,9 @@
 from datetime import date
 from django.test import TestCase
-from blog.models import Author, BlogPost
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from blog.models import Author
 
 
 class AuthorModelTest(TestCase):
@@ -16,3 +19,17 @@ class AuthorModelTest(TestCase):
     def test_string_representation(self):
         author = Author(first_name='Ana', last_name='Smith')
         self.assertEqual(str(author), 'Ana Smith')
+
+
+class AuthorAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.author1 = Author.objects.create(first_name="Mariam", last_name="Kipshidze")
+        self.author2 = Author.objects.create(first_name="Ana", last_name="Smith")
+
+    def test_list_authors(self):
+        response = self.client.get('/blog/author/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        names = [item['first_name'] for item in response.data['results']]
+        self.assertIn("Mariam", names)
+        self.assertIn("Ana", names)
